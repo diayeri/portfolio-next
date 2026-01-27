fcbfdfdf1e80479b036300c2ab91c57021ca7c11
diayeri <diayeri.dev@gmail.com>
2026-01-28 01:17:16 +0900

    fix: not-found 라우트 정리, SSR 오류 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,17 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { Button } from "@/components/Button";
-import { ArrowLeft } from "lucide-react";
+import { Home } from "lucide-react";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
   return (
     <div className="px-20 text-center mt-60">
       <h1 className="mb-4 text-4xl font-bold">404</h1>
       <p className="mb-12 text-xl">Page not found</p>
-      <Button size="sm" iconLeft={<ArrowLeft />} onClick={() => router.back()}>
-        Back
-      </Button>
+      <Link href="/">
+        <Button size="sm" iconLeft={<Home />}>
+          Home
+        </Button>
+      </Link>
     </div>
   );
 }
