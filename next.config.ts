5fea4ce39b4588beea72849445f071a2a677bbae
diayeri <diayeri.dev@gmail.com>
2026-01-28 02:51:53 +0900

    fix: build script 수정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    NEXT_PUBLIC_BUILD_DATE: new Date().toISOString(),
+  },
 };
 
 export default nextConfig;
