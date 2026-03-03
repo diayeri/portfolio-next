bf8ec552ffb1862d840a07810f5cacfbe03e79f7
diayeri <diayeri.dev@gmail.com>
2026-03-03 23:40:57 +0900

    refactor: Page, Home 역할 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,31 @@ import Portfolio from "@/components/Portfolio";
 import { HeroProvider } from "@/context/HeroContext";
 import { RoleProvider } from "@/context/RoleContext";
 
-const Home = ({ searchParams }: { searchParams: { role?: string } }) => {
-  const roleKey = searchParams.role === "design" ? "design" : "develop";
+type HomeProps = {
+  roleKey: "design" | "develop";
+};
 
+function Home({ roleKey }: HomeProps) {
   return (
     <RoleProvider roleKey={roleKey}>
       <div className="relative flex flex-col items-center justify-center">
-        {/* Hero */}
         <HeroProvider>
           <Hero />
         </HeroProvider>
-        {/* Core Competencies */}
         <Intro />
-        {/* Projects */}
         <Portfolio />
-        {/* Career History */}
         <Timeline />
       </div>
     </RoleProvider>
   );
-};
+}
 
-export default Home;
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { role?: string };
+}) {
+  const roleKey = searchParams.role === "design" ? "design" : "develop";
+
+  return <Home roleKey={roleKey} />;
+}
