6f1c47bc7d296da0a86e9230ecf1ba4c136483b3
diayeri <diayeri.dev@gmail.com>
2026-03-04 00:00:04 +0900

    fix: 파비콘 파일 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,10 +40,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    apple: "/apple-icon-180x180.png",
+    apple: "/favicon/apple-icon-180x180.png",
   },
 };
 
