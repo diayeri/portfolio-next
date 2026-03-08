aa1e08f1ddb3107654b5fea1be6e9e1081700265
diayeri <diayeri.dev@gmail.com>
2026-03-09 03:08:32 +0900

    style: featured projects 영역 확장 강조, 반응형 안내 사이즈 변경

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -28,17 +28,29 @@ const Portfolio = () => {
   }, [featuredProjects]);
 
   return (
-    <section className="w-full px-10 py-20 bg-black">
-      <div className="max-w-[1400px] mx-auto">
-        <div className="flex justify-between w-full">
-          <motion.h2
-            {...fadeUp(0, false)}
-            className="mr-auto text-5xl font-bold text-white"
-          >
-            Featured Projects
-          </motion.h2>
+    <section className="w-full px-0 py-20 bg-black">
+      <div className="flex items-center my-10">
+        {/* 미리보기 영역 */}
+        <div className="w-1/2 h-[600px] pl-12 rounded overflow-hidden flex items-center justify-center">
+          <NextImage
+            src={activeProject.featured!.cover}
+            alt={activeProject.title}
+            className="object-contain object-center w-full h-full transition-all duration-300"
+            width={800}
+            height={800}
+          />
+        </div>
+        {/* 대표 프로젝트 리스트 */}
+        <ul className="flex flex-col w-1/2 gap-0 pl-12">
+          <div className="flex justify-between w-full mb-12">
+            <motion.h2
+              {...fadeUp(0, false)}
+              className="mr-auto text-5xl font-bold text-white"
+            >
+              Featured Projects
+            </motion.h2>
 
-          {/* <Button
+            {/* <Button
             size='sm'
             variant='outline'
             className='hover:text-white hover:border-white'
@@ -47,51 +59,34 @@ const Portfolio = () => {
           >
             All Projects
           </Button> */}
-        </div>
-        <div className="flex items-center mt-10">
-          {/* 미리보기 영역 */}
-          <div className="w-1/2 h-[520px] pr-8 rounded overflow-hidden flex items-center justify-center">
-            <NextImage
-              src={activeProject.featured!.cover}
-              alt={activeProject.title}
-              className="object-contain object-center w-full h-full transition-all duration-300"
-              width={600}
-              height={600}
-            />
           </div>
-          {/* 대표 프로젝트 리스트 */}
-          <ul className="flex flex-col w-1/2 gap-0 pl-12">
-            {featuredProjects.map((project) => (
-              <li
-                key={project.id}
-                onMouseEnter={() => setActiveProject(project)}
-              >
-                <a href={`/projects/${project.id}`}>
-                  <div className="flex items-center py-6 border-b border-gray-500">
-                    <h3
-                      className={`text-3xl font-semibold transition-colors duration-200 ${
-                        activeProject.id === project.id
-                          ? "text-white"
-                          : "text-gray-700"
-                      }`}
-                    >
-                      {project.title}
-                    </h3>
-                    <span
-                      className={`px-2 py-1 ml-5 text-sm font-normal rounded bg-white/20${
-                        activeProject.id === project.id
-                          ? " text-white"
-                          : " text-gray-500"
-                      }`}
-                    >
-                      {roleData.replaceRole(project.category)}
-                    </span>
-                  </div>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+          {featuredProjects.map((project) => (
+            <li key={project.id} onMouseEnter={() => setActiveProject(project)}>
+              <a href={`/projects/${project.id}`}>
+                <div className="flex items-center py-6 border-b border-gray-500">
+                  <h3
+                    className={`text-3xl font-semibold transition-colors duration-200 ${
+                      activeProject.id === project.id
+                        ? "text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    {project.title}
+                  </h3>
+                  <span
+                    className={`px-2 py-1 ml-5 text-sm font-normal rounded bg-white/20${
+                      activeProject.id === project.id
+                        ? " text-white"
+                        : " text-gray-500"
+                    }`}
+                  >
+                    {roleData.replaceRole(project.category)}
+                  </span>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
